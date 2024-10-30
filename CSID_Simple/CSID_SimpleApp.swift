5fcf54507890a93a88cbc6476e4d861d5ec848e8
vincemuller <vmuller2529@gmail.com>
2024-10-29 22:46:09 -0700

    Initial Commit

---
+++ CSID_Simple/CSID_SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSID_SimpleApp.swift
+//  CSID_Simple
+//
+//  Created by Vince Muller on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CSID_SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
