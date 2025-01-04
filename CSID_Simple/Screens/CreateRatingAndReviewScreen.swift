7281a2bdcfc50a6613b4b834bf7868b8b64f4e7e
vincemuller <vmuller2529@gmail.com>
2025-01-04 08:43:47 -0700

    starting to build out saved lists
    built data models for savedlists and savedlistitems
    
    deployed to backend and pull updates to project so model schema's could be updated
    
    started working on UI design and workflow

---
+++ CSID_Simple/Screens/CreateRatingAndReviewScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CreateRatingAndReviewScreen.swift
+//  CSID_Simple
+//
+//  Created by Vince Muller on 12/31/24.
+//
+
+import SwiftUI
+
+struct CreateRatingAndReviewScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateRatingAndReviewScreen()
+}
